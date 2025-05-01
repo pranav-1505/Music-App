16f1f19137db3b46ffd0076812aa10649752b1b6
pranav-1505 <pranavpatil655@gmail.com>
2025-05-01 11:27:56 +0530

    Initial commit

---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,11 @@
+import { FaPauseCircle, FaPlayCircle} from 'react-icons/fa'
+
+const PlayPause = ({song,
+  isPlaying,
+  activeSong,
+  handlePause,
+  handlePlay,}) => (
+    isPlaying && activeSong?.id === song.id ? (<FaPauseCircle size={35} className='text-gray-300' onClick={handlePause}/>) : (<FaPlayCircle size={35} className='text-gray-300' onClick={handlePlay}/>)
+);
+
+export default PlayPause;
